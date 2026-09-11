fb0c787160fb3379361dc4df720816618996cddb
peter wilson <pwilson@unilink.com>
2026-09-11 10:51:49 +0100

    minor dependency update

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -9,6 +9,5 @@ export default configureAllowedScripts({
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/protobufjs@7.6.6': 'ALLOW',
     'node_modules/unrs-resolver@1.12.2': 'ALLOW',
-    'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
   },
 })
